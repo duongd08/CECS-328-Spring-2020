0b3a141815a1abbcbac76646abe204d9a618957d
Daniel Duong <duongdaniel08@gmail.com>
2020-09-10 21:48:48 -0700

    Update Info.java

--- SONIC/MonthlyPromo/Info.java
+++ SONIC/MonthlyPromo/Info.java
@@ -1,22 +1,53 @@
 public class Info {
 
+import java.util.*;
+  
 public static void main (String args[]) {
 
 
 String GM = "Sebastian Cortez";
+  
+  
+String jobTitle = " ";
+  
+System.out.println("GM: " + "please enter a job description for the employees");
+jobTitle = in.nextLine();
+    
+  
 
-
-
+switch (jobTitle) {
+    
+  case "carhop": 
+    jobTitle = "carhop";
+    break;
+    
+    
+  case "shift manager":
+    jobTitle = "shift manager";
+    break;
+    
+    
+  case "asst mngr":
+    jobTitle = "assitant manager";
+    break;
+    
+  case "cook":
+    jobTitle = "cook";
+    break;
+    
+  case "manager":
+    jobTitle = "manager";
+    
+  default:
+    jobTitle = "carhop";
+    
+    
+    
 }
 
 
 
-
-
-
-
-
-
+}
 
 
 }
