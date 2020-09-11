89bf3d48c1b312b36e1b5b7d719626e1bc10e6e2
Daniel Duong <duongdaniel08@gmail.com>
2020-09-10 22:07:34 -0700

    Update Info.java

--- SONIC/MonthlyPromo/Info.java
+++ SONIC/MonthlyPromo/Info.java
@@ -1,53 +1,98 @@
-public class Info {
+package work;
 
 import java.util.*;
-  
-public static void main (String args[]) {
+
+public class Info {
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        String GM = "Sebastian Cortez";
+
+        String jobTitle = " ";
+
+        System.out.println(GM + " please enter a job description for that particular employee: ");
+        jobTitle = in.nextLine().toLowerCase();
+
+        switch (jobTitle) {
+
+            case "carhop":
+                jobTitle = "carhop";
+                break;
+
+            case "shift manager":
+                jobTitle = "shift manager";
+                break;
+
+            case "asst mngr":
+                jobTitle = "assistant manager";
+                break;
+
+            case "cook":
+                jobTitle = "cook";
+                break;
+
+            case "manager":
+                jobTitle = "manager";
+                break;
+
+            default:
+                jobTitle = "carhop";
+
+        }
+        System.out.println("Job Description: " + jobTitle);
 
 
-String GM = "Sebastian Cortez";
-  
-  
-String jobTitle = " ";
-  
-System.out.println("GM: " + "please enter a job description for the employees");
-jobTitle = in.nextLine();
-    
-  
+        String position;
 
-switch (jobTitle) {
-    
-  case "carhop": 
-    jobTitle = "carhop";
-    break;
-    
-    
-  case "shift manager":
-    jobTitle = "shift manager";
-    break;
-    
-    
-  case "asst mngr":
-    jobTitle = "assitant manager";
-    break;
-    
-  case "cook":
-    jobTitle = "cook";
-    break;
-    
-  case "manager":
-    jobTitle = "manager";
-    
-  default:
-    jobTitle = "carhop";
-    
-    
-    
-}
+        System.out.println(GM + " please enter a job description for that particular employee: ");
+
+        position = in.nextLine().toLowerCase();
+
+        switch (position) {
+
+            case "supervisor":
+                position = "Supervisor";
+                break;
+
+            case "gm":
+                position = "General Manager";
+                break;
+
+            case "associate manager":
+                position = "Assistant Manager";
+                break;
+
+            case "shift leader":
+                position = "Shift Leader";
+                break;
+
+            case "crew leader":
+                position = "Crew Leader";
+
+            case "crew kitchen":
+                position = "Crew - Kitchen";
+                break;
+
+            case "crew drive through":
+                position = "Crew - Drive Through";
+                break;
+
+            case "crew expeditor ":
+                position = "Crew - Expeditor";
+                break;
+
+            case "Crew":
+                position = "Crew";
+                break;
+
+            default:
+                position = "Crew";
 
 
+        }
+        System.out.println("Position: " + position);
 
-}
-
-
-}
+    }
+    }
